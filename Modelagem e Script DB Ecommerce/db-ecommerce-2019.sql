c63e98fcbc5d8d463fb1f86275636614f94c31b8
eduardooantonio <eduardo.silva5@universo.univates.br>
2021-03-16 16:17:30 -0300

    inicio do projeto

---
+++ Modelagem e Script DB Ecommerce/db-ecommerce-2019.sql
@@ -0,0 +1,274 @@
+CREATE DATABASE  IF NOT EXISTS `db_ecommerce` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `db_ecommerce`;
+
+DROP TABLE IF EXISTS `db_ecommerce`.`tb_persons`;
+
+CREATE TABLE `db_ecommerce`.`tb_persons` (
+  `idperson` int(11) NOT NULL AUTO_INCREMENT,
+  `desperson` varchar(64) NOT NULL,
+  `desemail` varchar(128) DEFAULT NULL,
+  `nrphone` bigint(20) DEFAULT NULL,
+  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idperson`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+INSERT INTO `db_ecommerce`.`tb_persons` VALUES (1,'JoÃ£o Rangel','admin@hcode.com.br',2147483647,'2017-03-01 03:00:00'),(7,'Suporte','suporte@hcode.com.br',1112345678,'2017-03-15 16:10:27');
+
+DROP TABLE IF EXISTS `db_ecommerce`.`tb_users`;
+
+CREATE TABLE `db_ecommerce`.`tb_users` (
+  `iduser` int(11) NOT NULL AUTO_INCREMENT,
+  `idperson` int(11) NOT NULL,
+  `deslogin` varchar(64) NOT NULL,
+  `despassword` varchar(256) NOT NULL,
+  `inadmin` tinyint(4) NOT NULL DEFAULT '0',
+  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`iduser`),
+  KEY `FK_users_persons_idx` (`idperson`),
+  CONSTRAINT `fk_users_persons` FOREIGN KEY (`idperson`) REFERENCES `tb_persons` (`idperson`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+INSERT INTO `db_ecommerce`.`tb_users` VALUES (1,1,'admin','$2y$12$YlooCyNvyTji8bPRcrfNfOKnVMmZA9ViM2A3IpFjmrpIbp5ovNmga',1,'2017-03-13 03:00:00'),(7,7,'suporte','$2y$12$HFjgUm/mk1RzTy4ZkJaZBe0Mc/BA2hQyoUckvm.lFa6TesjtNpiMe',1,'2017-03-15 16:10:27');
+
+DROP TABLE IF EXISTS `db_ecommerce`.`tb_products`;
+
+CREATE TABLE `db_ecommerce`.`tb_products` (
+  `idproduct` int(11) NOT NULL AUTO_INCREMENT,
+  `desproduct` varchar(64) NOT NULL,
+  `vlprice` decimal(10,2) NOT NULL,
+  `vlwidth` decimal(10,2) NOT NULL,
+  `vlheight` decimal(10,2) NOT NULL,
+  `vllength` decimal(10,2) NOT NULL,
+  `vlweight` decimal(10,2) NOT NULL,
+  `desurl` varchar(128) NOT NULL,
+  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idproduct`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `db_ecommerce`.`tb_products` VALUES (1,'Smartphone Android 7.0',999.95,75.00,151.00,80.00,167.00,'smartphone-android-7.0','2017-03-13 03:00:00'),(2,'SmartTV LED 4K',3925.99,917.00,596.00,288.00,8600.00,'smarttv-led-4k','2017-03-13 03:00:00'),(3,'Notebook 14\" 4GB 1TB',1949.99,345.00,23.00,30.00,2000.00,'notebook-14-4gb-1tb','2017-03-13 03:00:00');
+
+DROP TABLE IF EXISTS `db_ecommerce`.`tb_addresses`;
+
+CREATE TABLE `db_ecommerce`.`tb_addresses` (
+  `idaddress` int(11) NOT NULL AUTO_INCREMENT,
+  `idperson` int(11) NOT NULL,
+  `desaddress` varchar(128) NOT NULL,
+  `descomplement` varchar(32) DEFAULT NULL,
+  `descity` varchar(32) NOT NULL,
+  `desstate` varchar(32) NOT NULL,
+  `descountry` varchar(32) NOT NULL,
+  `nrzipcode` int(11) NOT NULL,
+  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idaddress`),
+  KEY `fk_addresses_persons_idx` (`idperson`),
+  CONSTRAINT `fk_addresses_persons` FOREIGN KEY (`idperson`) REFERENCES `tb_persons` (`idperson`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `db_ecommerce`.`tb_carts`;
+
+CREATE TABLE `db_ecommerce`.`tb_carts` (
+  `idcart` int(11) NOT NULL,
+  `dessessionid` varchar(64) NOT NULL,
+  `iduser` int(11) DEFAULT NULL,
+  `idaddress` int(11) DEFAULT NULL,
+  `vlfreight` decimal(10,2) DEFAULT NULL,
+  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idcart`),
+  KEY `FK_carts_users_idx` (`iduser`),
+  KEY `fk_carts_addresses_idx` (`idaddress`),
+  CONSTRAINT `fk_carts_addresses` FOREIGN KEY (`idaddress`) REFERENCES `tb_addresses` (`idaddress`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_carts_users` FOREIGN KEY (`iduser`) REFERENCES `tb_users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `db_ecommerce`.`tb_cartsproducts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `db_ecommerce`.`tb_cartsproducts` (
+  `idcartproduct` int(11) NOT NULL AUTO_INCREMENT,
+  `idcart` int(11) NOT NULL,
+  `idproduct` int(11) NOT NULL,
+  `dtremoved` datetime NOT NULL,
+  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idcartproduct`),
+  KEY `FK_cartsproducts_carts_idx` (`idcart`),
+  KEY `FK_cartsproducts_products_idx` (`idproduct`),
+  CONSTRAINT `fk_cartsproducts_carts` FOREIGN KEY (`idcart`) REFERENCES `tb_carts` (`idcart`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_cartsproducts_products` FOREIGN KEY (`idproduct`) REFERENCES `tb_products` (`idproduct`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `db_ecommerce`.`tb_categories`;
+
+CREATE TABLE `db_ecommerce`.`tb_categories` (
+  `idcategory` int(11) NOT NULL AUTO_INCREMENT,
+  `descategory` varchar(32) NOT NULL,
+  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idcategory`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `db_ecommerce`.`tb_orders`;
+
+DROP TABLE IF EXISTS `db_ecommerce`.`tb_ordersstatus`;
+
+CREATE TABLE `db_ecommerce`.`tb_ordersstatus` (
+  `idstatus` int(11) NOT NULL AUTO_INCREMENT,
+  `desstatus` varchar(32) NOT NULL,
+  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idstatus`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+INSERT INTO `db_ecommerce`.`tb_ordersstatus` VALUES (1,'Em Aberto','2017-03-13 03:00:00'),(2,'Aguardando Pagamento','2017-03-13 03:00:00'),(3,'Pago','2017-03-13 03:00:00'),(4,'Entregue','2017-03-13 03:00:00');
+
+CREATE TABLE `db_ecommerce`.`tb_orders` (
+  `idorder` int(11) NOT NULL AUTO_INCREMENT,
+  `idcart` int(11) NOT NULL,
+  `iduser` int(11) NOT NULL,
+  `idstatus` int(11) NOT NULL,
+  `vltotal` decimal(10,2) NOT NULL,
+  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idorder`),
+  KEY `FK_orders_carts_idx` (`idcart`),
+  KEY `FK_orders_users_idx` (`iduser`),
+  KEY `fk_orders_ordersstatus_idx` (`idstatus`),
+  CONSTRAINT `fk_orders_carts` FOREIGN KEY (`idcart`) REFERENCES `tb_carts` (`idcart`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_orders_ordersstatus` FOREIGN KEY (`idstatus`) REFERENCES `tb_ordersstatus` (`idstatus`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_orders_users` FOREIGN KEY (`iduser`) REFERENCES `tb_users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `db_ecommerce`.`tb_productscategories`;
+
+CREATE TABLE `db_ecommerce`.`tb_productscategories` (
+  `idcategory` int(11) NOT NULL,
+  `idproduct` int(11) NOT NULL,
+  PRIMARY KEY (`idcategory`,`idproduct`),
+  KEY `fk_productscategories_products_idx` (`idproduct`),
+  CONSTRAINT `fk_productscategories_categories` FOREIGN KEY (`idcategory`) REFERENCES `tb_categories` (`idcategory`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_productscategories_products` FOREIGN KEY (`idproduct`) REFERENCES `tb_products` (`idproduct`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `db_ecommerce`.`tb_userslogs`;
+
+CREATE TABLE `db_ecommerce`.`tb_userslogs` (
+  `idlog` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) NOT NULL,
+  `deslog` varchar(128) NOT NULL,
+  `desip` varchar(45) NOT NULL,
+  `desuseragent` varchar(128) NOT NULL,
+  `dessessionid` varchar(64) NOT NULL,
+  `desurl` varchar(128) NOT NULL,
+  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idlog`),
+  KEY `fk_userslogs_users_idx` (`iduser`),
+  CONSTRAINT `fk_userslogs_users` FOREIGN KEY (`iduser`) REFERENCES `tb_users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `db_ecommerce`.`tb_userspasswordsrecoveries`;
+
+CREATE TABLE `db_ecommerce`.`tb_userspasswordsrecoveries` (
+  `idrecovery` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) NOT NULL,
+  `desip` varchar(45) NOT NULL,
+  `dtrecovery` datetime DEFAULT NULL,
+  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idrecovery`),
+  KEY `fk_userspasswordsrecoveries_users_idx` (`iduser`),
+  CONSTRAINT `fk_userspasswordsrecoveries_users` FOREIGN KEY (`iduser`) REFERENCES `tb_users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+INSERT INTO `db_ecommerce`.`tb_userspasswordsrecoveries` VALUES (1,7,'127.0.0.1',NULL,'2017-03-15 16:10:59'),(2,7,'127.0.0.1','2017-03-15 13:33:45','2017-03-15 16:11:18'),(3,7,'127.0.0.1','2017-03-15 13:37:35','2017-03-15 16:37:12');
+
+DELIMITER ;;
+CREATE PROCEDURE `db_ecommerce`.`sp_userspasswordsrecoveries_create`(
+piduser INT,
+pdesip VARCHAR(45)
+)
+BEGIN
+  
+  INSERT INTO tb_userspasswordsrecoveries (iduser, desip)
+    VALUES(piduser, pdesip);
+    
+    SELECT * FROM tb_userspasswordsrecoveries
+    WHERE idrecovery = LAST_INSERT_ID();
+    
+END ;;
+DELIMITER ;
+
+DELIMITER ;;
+CREATE PROCEDURE `db_ecommerce`.`sp_usersupdate_save`(
+piduser INT,
+pdesperson VARCHAR(64), 
+pdeslogin VARCHAR(64), 
+pdespassword VARCHAR(256), 
+pdesemail VARCHAR(128), 
+pnrphone BIGINT, 
+pinadmin TINYINT
+)
+BEGIN
+  
+    DECLARE vidperson INT;
+    
+  SELECT idperson INTO vidperson
+    FROM tb_users
+    WHERE iduser = piduser;
+    
+    UPDATE tb_persons
+    SET 
+    desperson = pdesperson,
+        desemail = pdesemail,
+        nrphone = pnrphone
+  WHERE idperson = vidperson;
+    
+    UPDATE tb_users
+    SET
+    deslogin = pdeslogin,
+        despassword = pdespassword,
+        inadmin = pinadmin
+  WHERE iduser = piduser;
+    
+    SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = piduser;
+    
+END ;;
+DELIMITER ;
+
+DELIMITER ;;
+CREATE PROCEDURE `db_ecommerce`.`sp_users_delete`(
+piduser INT
+)
+BEGIN
+  
+    DECLARE vidperson INT;
+    
+  SELECT idperson INTO vidperson
+    FROM tb_users
+    WHERE iduser = piduser;
+    
+    DELETE FROM tb_users WHERE iduser = piduser;
+    DELETE FROM tb_persons WHERE idperson = vidperson;
+    
+END ;;
+DELIMITER ;
+
+DELIMITER ;;
+CREATE PROCEDURE `db_ecommerce`.`sp_users_save`(
+pdesperson VARCHAR(64), 
+pdeslogin VARCHAR(64), 
+pdespassword VARCHAR(256), 
+pdesemail VARCHAR(128), 
+pnrphone BIGINT, 
+pinadmin TINYINT
+)
+BEGIN
+  
+    DECLARE vidperson INT;
+    
+  INSERT INTO tb_persons (desperson, desemail, nrphone)
+    VALUES(pdesperson, pdesemail, pnrphone);
+    
+    SET vidperson = LAST_INSERT_ID();
+    
+    INSERT INTO tb_users (idperson, deslogin, despassword, inadmin)
+    VALUES(vidperson, pdeslogin, pdespassword, pinadmin);
+    
+    SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = LAST_INSERT_ID();
+    
+END ;;
+DELIMITER ;
---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'eduardooantonio/ecommerce',
+  ),
+  'versions' => 
+  array (
+    'eduardooantonio/ecommerce' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v5.2.22',
+      'version' => '5.2.22.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b18cb98131bd83103ccb26a888fdfe3177b8a663',
+    ),
+    'rain/raintpl' => 
+    array (
+      'pretty_version' => '3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5bfdf4602b8adb0e80b222fa29ac559aa9c06abf',
+    ),
+    'slim/slim' => 
+    array (
+      'pretty_version' => '2.0.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3e95c9abbc57a8f023ab4dacebcab9dae4d9f1f0',
+    ),
+  ),
+);
